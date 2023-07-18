f481f73bd9b58f54822fa3e07773798459a6054a
stacksrahul <rahulborikar57@gmail.com>
2023-07-19 01:59:50 +0530

    Setup the database

--- Soccer/backend/index.js
+++ Soccer/backend/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
+// mongo connection
+mongoose.Promise = global.Promise;  //use promise library that's built into javascript
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// body-parser
+app.use(bodyparser.urlencoded({ extended: true }));     //use url-encoder when form submitted
+app.use(bodyparser.json());     //use json parser
+
+// main application
 app.get('/',(req,res) =>
     res.send(`Our application is running on port ${PORT}`)
 );
@@ -10,4 +22,3 @@ app.get('/',(req,res) =>
 app.listen(PORT, () =>
     console.log(`Your soccer server is running on port ${PORT}`)
 );
-  
